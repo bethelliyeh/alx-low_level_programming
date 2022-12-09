7dd5813178313731fa17a1fd2155870385dc6b3a
bethelliyeh <ainoko112@gmail.com>
2022-12-09 11:39:51 -0800

    tfc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Empty point
- * Retun: 0 (sucess)
+ * Return: 0 (sucess)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
