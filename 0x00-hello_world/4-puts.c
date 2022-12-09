58584c79ad08253db8bb3f4c6130b95b4a1b7775
bethelliyeh <ainoko112@gmail.com>
2022-12-09 10:42:49 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h> 
-/**  
- *  main - Entry  
- *  Return: Always 0
- */ 
-int main(void)                                                                                             
-{          
-	 puts("\"Programming is like building a multi lingual puzzle");                                                            
-		 return (0);
+#include <stad.h>
+/**
+ * main-entry point
+ *
+ * return: always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multi lingual puzzle");
+	return (0);
 }
