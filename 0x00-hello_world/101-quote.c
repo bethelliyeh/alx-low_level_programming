c8f49b9924c3d8a491afa0c067b6199dbf6d11f7
bethelliyeh <ainoko112@gmail.com>
2022-12-10 06:32:26 -0800

    good

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
+/** 
 *main - Entry
-*Return: Always 1
+*Return: Always 0
 */
+main(void)
 int main(void)
 {
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizof(float));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
 }
