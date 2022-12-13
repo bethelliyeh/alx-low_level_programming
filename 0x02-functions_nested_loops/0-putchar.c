4d3dd7a129d4bd254349b09932313c27f6f64ddb
bethelliyeh <ainoko112@gmail.com>
2022-12-13 08:44:10 -0800

    pribb

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,24 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point
+ * main - program that prints
  * description: working with standard library
  * print new line
  *
- * Return: 0
- **/
+ * Return:Always 0
+ */
 
 int main(void)
 {
-       putchar('D');
+       putchar(");
+       _putchar('p')
+       _putchar('u');
+       _putchar('t');
+       _putchar('c');
+       _putchar('h');
+       _putchar('a');
+       _putchar('r');
+       _putchar('\n');
+
        return (0);
 }
