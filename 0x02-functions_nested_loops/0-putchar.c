241b4a593b168f6a3e6b8eb38cdb3692489bbc27
bethelliyeh <ainoko112@gmail.com>
2022-12-13 07:50:07 -0800

    prixer

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - print line
+ * description print char
+ * print new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+       putchar('D');
+       return (0);
+}
